bff776701f6c02f1c31854056aa76d76ab757f6d
LaurePiechaczyk <laure.piecha@gmail.com>
2022-03-30 16:29:34 +0000

    change version react-chart-2

--- dashboard/src/Components/AllChart/BarChart.js
+++ dashboard/src/Components/AllChart/BarChart.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {Bar} from 'react-chartjs-2';
+
 import fulldata from "../../context/fulldata";
 
 export default function BarChart() {
     console.log(fulldata)
 
 
+    
+
+
   return (
     <div className="chart-container">
-    
- 
+      <Bar />
+
     </div>
   )
 }
